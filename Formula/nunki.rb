3ada3b4ed4276e40f7ed6dfab885497235d55c93
Kal Sadaram <kal@netlabs.com.au>
2026-09-21 07:29:21 -0500

    nunki 0.4.0

--- Formula/nunki.rb
+++ Formula/nunki.rb
@@ -1,28 +1,28 @@
 class Nunki < Formula
   desc "Architecture docs from source code, verified against the commit"
   homepage "https://github.com/sadaramk/nunki"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/sadaramk/nunki/releases/download/v0.3.0/nunki-0.3.0-aarch64-apple-darwin.tar.gz"
-      sha256 "b33f72e569e3d821f1e8f17f01a73e5ef80e711f7e000b627353ecb1f7949e7e"
+      url "https://github.com/sadaramk/nunki/releases/download/v0.4.0/nunki-0.4.0-aarch64-apple-darwin.tar.gz"
+      sha256 "8e6a2ea365ca1e5b9c42a096d69dd1c7a16044f56a61d15c86b58e890997e433"
     end
     on_intel do
-      url "https://github.com/sadaramk/nunki/releases/download/v0.3.0/nunki-0.3.0-x86_64-apple-darwin.tar.gz"
-      sha256 "a6f15d82808cca08e6676ae33ba6ed7c6784a907feab7dacbd540e1019d0472d"
+      url "https://github.com/sadaramk/nunki/releases/download/v0.4.0/nunki-0.4.0-x86_64-apple-darwin.tar.gz"
+      sha256 "092e1baa8c57a1aa3889843fd3daf7ae4bf6cd3a6e1a84d84535c04a02ca40fc"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/sadaramk/nunki/releases/download/v0.3.0/nunki-0.3.0-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "85f5c719da0ab11ae929bc5862272fd5c5d146785d7dce73f0368ab9f27b2a90"
+      url "https://github.com/sadaramk/nunki/releases/download/v0.4.0/nunki-0.4.0-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "d7583ab276eb46f9435ec800bdf44d3f06d573c3bdd1c86e62b3b070f7ebffc8"
     end
     on_intel do
-      url "https://github.com/sadaramk/nunki/releases/download/v0.3.0/nunki-0.3.0-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "391c98766e6e22fd9716b2af65bfb40a7c500bc0d1564ec9aafb9d860787f02e"
+      url "https://github.com/sadaramk/nunki/releases/download/v0.4.0/nunki-0.4.0-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "e0d9ed6e9de3efa45423300589670d97b07b67c6f0852a14fb7f05c4addc51e6"
     end
   end
 
